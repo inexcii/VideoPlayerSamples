5edc719ec56d253fe045070ad665b723fbf9e3cf
g.shu <g.shu@suvt.co.jp>
2018-01-30 17:27:41 +0900

    feat(asset): change to retrieve asset's property asynchronously

--- VideoPlayerSamples/ViewController.m
+++ VideoPlayerSamples/ViewController.m
@@ -22,6 +22,11 @@
 
 #pragma mark - Override
 
+- (void)dealloc
+{
+    NSLog(@"dealloc");
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -42,6 +47,7 @@
             case AVPlayerItemStatusReadyToPlay:
             {
                 NSLog(@"playerItem's duration: %lf", CMTimeGetSeconds(playerItem.duration));
+                [playerItem removeObserver:self forKeyPath:NSStringFromSelector(@selector(status)) context:context];
                 
                 [self.player play];
                 NSLog(@"play the video");
@@ -71,11 +77,40 @@
     AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:videoUrl options:nil];
     NSLog(@"asset created");
     
-//    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
-//        NSLog(@"asset's duration is :%lf", CMTimeGetSeconds(asset.duration));
-//    });
+    // retrieve asset's duration, video track and its size asynchronously
+    NSArray *keys = @[@"duration", @"tracks"];
+    [asset loadValuesAsynchronouslyForKeys:keys completionHandler:^{
+        NSError *error = nil;
+        AVKeyValueStatus trackStatusDuration = [asset statusOfValueForKey:@"duration" error:&error];
+        switch (trackStatusDuration) {
+            case AVKeyValueStatusLoaded:
+            {
+                NSLog(@"duration is %lf", CMTimeGetSeconds(asset.duration));
+            }
+                break;
+            default:
+            {
+                NSLog(@"duration cannot be retrieved due to the AVKeyValueStatus of %ld", (long)trackStatusDuration);
+            }
+                break;
+        }
+        
+        AVKeyValueStatus trackStatusTracks = [asset statusOfValueForKey:@"tracks" error:&error];
+        switch (trackStatusTracks) {
+            case AVKeyValueStatusLoaded:
+            {
+                AVAssetTrack *track = (AVAssetTrack *)[asset tracksWithMediaType:AVMediaTypeVideo][0];
+                NSLog(@"tracks are: %@", NSStringFromCGSize(track.naturalSize));
+            }
+                break;
+                
+            default:
+                NSLog(@"tracks cannot be retrieved due to the AVKeyValueStatus of %ld", (long)trackStatusTracks);
+                break;
+        }
+    }];
     
-//    AVPlayerItem *playerItem = [AVPlayerItem playerItemWithURL:videoUrl];
+    
     AVPlayerItem *playerItem = [AVPlayerItem playerItemWithAsset:asset];
     NSLog(@"player item created");
     NSLog(@"playerItem's duration: %lf", CMTimeGetSeconds(playerItem.duration));
